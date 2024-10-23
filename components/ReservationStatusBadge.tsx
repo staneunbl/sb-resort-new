20578f6f85d447844629fc7ad0733666610b83b9
WRavagoLFT <wendell.ravago@linoflaptech.com>
2024-10-23 13:03:48 +0800

    add data cards for reservation table

--- components/ReservationStatusBadge.tsx
+++ components/ReservationStatusBadge.tsx
@@ -7,8 +7,9 @@ export default function ReservationStatusBadge({ status }: { status: string }) {
   const badgeColor = {
     Pending: "bg-gray-500",
     "Checked-In": "bg-green-500",
-    Done: "bg-black",
+    Done: "bg-blue-500",
     Cancelled: "bg-red-500",
+    "No-show": "bg-red-500",
     Booked: "bg-orange-500",
     Completed: "bg-blue-500",
   }[status];
