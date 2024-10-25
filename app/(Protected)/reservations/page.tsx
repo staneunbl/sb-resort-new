ab3e5a2f8344d426592102aabfeecd652e7f070e
WRavagoLFT <wendell.ravago@linoflaptech.com>
2024-10-25 15:34:31 +0800

    temporary removal of new module

--- app/(Protected)/reservations/page.tsx
+++ app/(Protected)/reservations/page.tsx
@@ -2,11 +2,10 @@ import ReservationController from "./ReservationController";
 import ReservationTable from "./ReservationTable";
 import BillingFormModal from "./BillingFormModal";
 import ReservationModal from "./ReservationModal";
-import AddReservationModal from "./AddReservationModal";
 export default function page() {
     return (
         <div >
-            <AddReservationModal></AddReservationModal>
+            {/* <AddReservationModal /> */}
             <ReservationModal />
             <BillingFormModal />
             <ReservationController />
